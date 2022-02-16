950acc031dcb52392144fbad4e948b7288c5bcce
bananaturtlesandwich <afcarreyette@gmail.com>
2022-02-16 14:30:33 +0000

    Add project files.

---
+++ UE4 Map Editor/Editor.cs
@@ -0,0 +1,15 @@
+namespace UE4_Map_Editor
+{
+    public partial class Editor : Form
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
