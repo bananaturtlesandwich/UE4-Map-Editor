d97d52b798cf6307d508e41cb22635db69f62ea1
bananaturtlesandwich <afcarreyette@gmail.com>
2022-02-17 12:47:13 +0000

    Layout Finished

--- Map Editor/Program.cs
+++ Map Editor/Program.cs
@@ -8,6 +8,8 @@ namespace Map_Editor
         [STAThread]
         static void Main()
         {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Editor());
         }
