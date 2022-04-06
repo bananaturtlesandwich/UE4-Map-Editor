b47153416ba21c026ea1ccb5c720f99f6b4d42b9
bananaturtlesandwich <afcarreyette@gmail.com>
2022-04-06 20:30:44 +0100

    Yet another redo

--- Map Editor/Editor.Designer.cs
+++ Map Editor/Editor.Designer.cs
@@ -1,27 +1,32 @@
-﻿namespace UE4MapEditor
+﻿using System.Windows.Forms;
+
+namespace Map_Editor
 {
     partial class Editor
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null)) components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
             base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -31,31 +36,26 @@
             this.Open = new System.Windows.Forms.ToolStripMenuItem();
             this.Save = new System.Windows.Forms.ToolStripMenuItem();
             this.SaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenMapDialog = new System.Windows.Forms.OpenFileDialog();
             this.Split = new System.Windows.Forms.SplitContainer();
             this.Sidebar = new System.Windows.Forms.SplitContainer();
-            this.Objects = new GL_EditorFramework.SceneListView();
-            this.Display = new GL_EditorFramework.GL_Core.GL_ControlModern();
-            this.Properties = new GL_EditorFramework.ObjectUIControl();
-            this.OpenMapDialog = new System.Windows.Forms.OpenFileDialog();
             this.MenuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Split)).BeginInit();
             this.Split.Panel1.SuspendLayout();
-            this.Split.Panel2.SuspendLayout();
             this.Split.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Sidebar)).BeginInit();
-            this.Sidebar.Panel1.SuspendLayout();
             this.Sidebar.SuspendLayout();
             this.SuspendLayout();
             // 
             // MenuStrip
             // 
-            this.MenuStrip.Font = new System.Drawing.Font("Segoe UI", 4F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MenuStrip.Font = new System.Drawing.Font("Segoe UI", 4F);
             this.MenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.FileToolStrip});
             this.MenuStrip.Location = new System.Drawing.Point(0, 0);
             this.MenuStrip.Name = "MenuStrip";
-            this.MenuStrip.Size = new System.Drawing.Size(1498, 33);
+            this.MenuStrip.Size = new System.Drawing.Size(1200, 28);
             this.MenuStrip.TabIndex = 0;
             this.MenuStrip.Text = "MenuStrip";
             // 
@@ -65,24 +65,23 @@
             this.Open,
             this.Save,
             this.SaveAs});
-            this.FileToolStrip.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FileToolStrip.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.FileToolStrip.Name = "FileToolStrip";
-            this.FileToolStrip.Size = new System.Drawing.Size(54, 29);
+            this.FileToolStrip.Size = new System.Drawing.Size(46, 24);
             this.FileToolStrip.Text = "File";
             // 
             // Open
             // 
             this.Open.Name = "Open";
             this.Open.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.Open.Size = new System.Drawing.Size(285, 34);
+            this.Open.Size = new System.Drawing.Size(233, 26);
             this.Open.Text = "Open";
-            this.Open.Click += new System.EventHandler(this.OpenMap);
             // 
             // Save
             // 
             this.Save.Name = "Save";
             this.Save.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.Save.Size = new System.Drawing.Size(285, 34);
+            this.Save.Size = new System.Drawing.Size(233, 26);
             this.Save.Text = "Save";
             // 
             // SaveAs
@@ -90,99 +89,42 @@
             this.SaveAs.Name = "SaveAs";
             this.SaveAs.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
             | System.Windows.Forms.Keys.S)));
-            this.SaveAs.Size = new System.Drawing.Size(285, 34);
+            this.SaveAs.Size = new System.Drawing.Size(233, 26);
             this.SaveAs.Text = "Save As";
-            // 
-            // Split
-            // 
-            this.Split.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Split.Location = new System.Drawing.Point(0, 33);
-            this.Split.Margin = new System.Windows.Forms.Padding(2);
-            this.Split.Name = "Split";
-            // 
-            // Split.Panel1
-            // 
-            this.Split.Panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.Split.Panel1.Controls.Add(this.Sidebar);
-            // 
-            // Split.Panel2
-            // 
-            this.Split.Panel2.Controls.Add(this.Display);
-            this.Split.Size = new System.Drawing.Size(1498, 1017);
-            this.Split.SplitterDistance = 386;
-            this.Split.TabIndex = 1;
-            // 
-            // Sidebar
-            // 
-            this.Sidebar.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Sidebar.Location = new System.Drawing.Point(0, 0);
-            this.Sidebar.Margin = new System.Windows.Forms.Padding(2);
-            this.Sidebar.Name = "Sidebar";
-            this.Sidebar.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // Sidebar.Panel1
-            // 
-            this.Sidebar.Panel1.Controls.Add(this.Objects);
-            this.Sidebar.Size = new System.Drawing.Size(386, 1017);
-            this.Sidebar.SplitterDistance = 440;
-            this.Sidebar.TabIndex = 0;
-            // 
-            // Sidebar.Panel2
-            //
-            this.Sidebar.Panel2.Controls.Add(this.Properties);
-            // 
-            // Objects
-            // 
-            this.Objects.AutoScroll = true;
-            this.Objects.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Objects.Location = new System.Drawing.Point(0, 0);
-            this.Objects.Margin = new System.Windows.Forms.Padding(12, 4, 12, 4);
-            this.Objects.Name = "Objects";
-            this.Objects.RootLists = ((System.Collections.Generic.Dictionary<string, System.Collections.IList>)(resources.GetObject("Objects.RootLists")));
-            this.Objects.Size = new System.Drawing.Size(386, 440);
-            this.Objects.ItemClicked += new GL_EditorFramework.ItemClickedEventHandler(this.FocusObject);
-            // 
-            // Display
-            // 
-            this.Display.BackColor = System.Drawing.Color.Black;
-            this.Display.CameraDistance = 10F;
-            this.Display.CameraTarget = ((OpenTK.Vector3)(resources.GetObject("Display.CameraTarget")));
-            this.Display.CamRotX = 0F;
-            this.Display.CamRotY = 0F;
-            this.Display.CurrentShader = null;
-            this.Display.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Display.Fov = 0.7853982F;
-            this.Display.Location = new System.Drawing.Point(0, 0);
-            this.Display.Margin = new System.Windows.Forms.Padding(5);
-            this.Display.Name = "Display";
-            this.Display.NormPickingDepth = 0F;
-            this.Display.ShowOrientationCube = true;
-            this.Display.Size = new System.Drawing.Size(1108, 1017);
-            this.Display.Stereoscopy = GL_EditorFramework.GL_Core.GL_ControlBase.StereoscopyType.DISABLED;
-            this.Display.VSync = false;
-            this.Display.ZFar = 32000F;
-            this.Display.ZNear = 0.32F;
-            // 
-            // Properties
-            // 
-            this.Properties.AutoScroll = true;
-            this.Properties.BackColor = System.Drawing.SystemColors.Control;
-            this.Properties.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Properties.Location = new System.Drawing.Point(0,0);
-            this.Properties.Margin = new System.Windows.Forms.Padding(6);
-            this.Properties.Name = "Properties";
-            this.Properties.Size = new System.Drawing.Size(398, 509);
             // 
             // OpenMapDialog
             // 
             this.OpenMapDialog.Filter = "\"Unreal map files|*.umap\"";
             this.OpenMapDialog.InitialDirectory = ".\\";
             // 
+            // Split
+            // 
+            this.Split.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Split.Location = new System.Drawing.Point(0, 28);
+            this.Split.Name = "Split";
+            // 
+            // Split.Panel1
+            // 
+            this.Split.Panel1.Controls.Add(this.Sidebar);
+            this.Split.Size = new System.Drawing.Size(1200, 652);
+            this.Split.SplitterDistance = 400;
+            this.Split.TabIndex = 1;
+            // 
+            // Sidebar
+            // 
+            this.Sidebar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Sidebar.Location = new System.Drawing.Point(0, 0);
+            this.Sidebar.Name = "Sidebar";
+            this.Sidebar.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.Sidebar.Size = new System.Drawing.Size(400, 652);
+            this.Sidebar.SplitterDistance = 270;
+            this.Sidebar.TabIndex = 0;
+            // 
             // Editor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1498, 1050);
+            this.ClientSize = new System.Drawing.Size(1200, 680);
             this.Controls.Add(this.Split);
             this.Controls.Add(this.MenuStrip);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -196,10 +138,8 @@
             this.MenuStrip.ResumeLayout(false);
             this.MenuStrip.PerformLayout();
             this.Split.Panel1.ResumeLayout(false);
-            this.Split.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.Split)).EndInit();
             this.Split.ResumeLayout(false);
-            this.Sidebar.Panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.Sidebar)).EndInit();
             this.Sidebar.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -214,13 +154,9 @@
         private ToolStripMenuItem Open;
         private ToolStripMenuItem Save;
         private ToolStripMenuItem SaveAs;
+        private OpenFileDialog OpenMapDialog;
         private SplitContainer Split;
         private SplitContainer Sidebar;
-
-        //GL Editor Controls
-        private GL_EditorFramework.GL_Core.GL_ControlModern Display;
-        private GL_EditorFramework.SceneListView Objects;
-        private GL_EditorFramework.ObjectUIControl Properties;
-        private OpenFileDialog OpenMapDialog;
     }
 }
+
